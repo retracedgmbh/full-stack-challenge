14dce9977eca6629bcbd7df11e54f40676c01ff8
Peter Merkert <37897665+PeterMerkert@users.noreply.github.com>
2022-03-12 12:10:59 +0200

    chore: retraced.co to retraced.com domain [TECHOPS-373] (#1)

--- frontend/src/components/Logo/index.jsx
+++ frontend/src/components/Logo/index.jsx
@@ -2,8 +2,8 @@ export const Logo = () => (
   <div style={{ width: "260px" }}>
     <img
       alt="retraced logo"
-      srcSet="https://cdn.retraced.co/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=128 1x, https://cdn.retraced.co/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=256 2x"
-      src="https://cdn.retraced.co/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=256"
+      srcSet="https://files.retraced.com/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=128 1x, https://cdn.retraced.co/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=256 2x"
+      src="https://files.retraced.com/static/homepage/secondary-logo-black.svg?auto=format&amp;fit=max&amp;w=256"
       decoding="async"
     />
   </div>
